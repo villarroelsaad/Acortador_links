5877f9041c2c9dfb7b56a204ef56f3a4744d64fa
Jose Villarroel <163937499+villarroelsaad@users.noreply.github.com>
2024-08-19 13:24:10 -0400

    validations

--- api/routes/rUsers.js
+++ api/routes/rUsers.js
@@ -3,6 +3,7 @@ import { UserController } from '../controllers/user.js'
 
 export const routerUser = Router()
 
+routerUser.get('/', UserController.home)
 routerUser.get('/:hash', UserController.hash)
 routerUser.get('/links/:id', UserController.links)
 routerUser.get('/logout', UserController.logOut)
