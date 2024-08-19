7cf4cd7a7d2371d4fcf7074f7ef95afa2776f05f
Jose Villarroel <163937499+villarroelsaad@users.noreply.github.com>
2024-08-19 13:16:38 -0400

    c

--- api/schemas/user.js
+++ api/schemas/user.js
@@ -4,11 +4,11 @@ const userSchema = z.object({
     username: z.string().min(5),
     email: z.string().min(15),
     password: z.string().min(10),
-    url: z.url()
+    url: z.string().url()
 })
 const urlSchema = z.object({
     id: z.number().int(),
-    url: z.url()
+    url: z.string().url()
 })
 
 export const validateUser = function (object) {
