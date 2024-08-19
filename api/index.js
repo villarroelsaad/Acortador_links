4a58b218c2bce3ad657e73d84d72cc8d68f2c6c8
Jose Villarroel <163937499+villarroelsaad@users.noreply.github.com>
2024-08-19 16:36:45 -0400

    fixes

--- api/index.js
+++ api/index.js
@@ -1,34 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import { session } from './config.js'
+import { corsOptions, session } from './config.js'
 import { routerUser } from './routes/rUsers.js'
 
 const app = express()
-const allowedOrigins = ['https://acortador-links-front.vercel.app']
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}
-
-// Aplica CORS a todas las rutas
-app.use(cors(corsOptions))
-
-// Maneja solicitudes preflight
-app.options('*', cors(corsOptions))
 
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors(corsOptions))
 app.use(session)
 
 app.use('/', routerUser)
