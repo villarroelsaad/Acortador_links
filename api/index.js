a31bc025fc277c02be057798e9409c1c9613fb04
villarroelsaad <villarroelsaad@gmail.com>
2024-08-19 14:06:12 -0400

    cors

--- api/index.js
+++ api/index.js
@@ -5,12 +5,6 @@ import { session } from './config.js'
 import { routerUser } from './routes/rUsers.js'
 
 const app = express()
-
-app.disable('x-powered-by')
-app.use(express.json())
-app.use(cookieParser())
-const allowedOrigins = ['https://acortador-links-front.vercel.app']
-
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -21,7 +15,7 @@ const corsOptions = {
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Permite el uso de cookies
+  credentials: true
 }
 
 // Aplica CORS a todas las rutas
@@ -29,6 +23,12 @@ app.use(cors(corsOptions))
 
 // Maneja solicitudes preflight
 app.options('*', cors(corsOptions))
+
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(cookieParser())
+
+const allowedOrigins = ['https://acortador-links-front.vercel.app']
 
 app.use(session)
 
