e5ba6d7c2eb5e183e624ce3d764ca35e3feb7b61
villarroelsaad <villarroelsaad@gmail.com>
2024-08-19 12:10:42 -0400

    api-structure

--- api/config.js
+++ api/config.js
@@ -1,1 +1,37 @@
+import mysql from 'mysql2/promise'
+import jsonwebtoken from 'jsonwebtoken'
+
 export const Secret = 'kjsdafhksdjahflksadfhlksjdafh'
+const allowedOrigins = ['https://acortador-links-front.vercel.app']
+export const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            // Permitir solicitudes desde los orígenes permitidos o solicitudes sin origen (como las de herramientas de prueba)
+            callback(null, true)
+        } else {
+            // Bloquear solicitudes desde orígenes no permitidos
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    methods: ['GET', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Permite enviar cookies en solicitudes
+}
+
+export const connection = await mysql.createConnection({
+    host: 'betxwuucvkixjqb9vapw-mysql.services.clever-cloud.com',
+    user: 'u7rnrdilogdxm8ks',
+    password: '5GfGWtuHc3bo34PbSRUK',
+    database: 'betxwuucvkixjqb9vapw'
+})
+
+export const session = (req, res, next) => {
+    const token = req.cookies.access_token
+    req.session = { user: null }
+
+    try {
+        const data = jsonwebtoken.verify(token, Secret)
+        req.session.user = data
+    } catch { }
+    next()
+}
--- api/index.js
+++ api/index.js
@@ -1,156 +1,18 @@
 import express from 'express'
-import mysql from 'mysql2/promise'
-import crypto from 'node:crypto'
 import cors from 'cors'
-import jsonwebtoken from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import cookieParser from 'cookie-parser'
-import { Secret } from './config.js'
+import { corsOptions, session } from './config.js'
+import { routerUser } from './routes/rUsers.js'
 
 const app = express()
-
-// Conectar a la base de datos
-const connection = await mysql.createConnection({
-  host: 'betxwuucvkixjqb9vapw-mysql.services.clever-cloud.com',
-  user: 'u7rnrdilogdxm8ks',
-  password: '5GfGWtuHc3bo34PbSRUK',
-  database: 'betxwuucvkixjqb9vapw'
-})
 
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = ['https://acortador-links-front.vercel.app']
-
-// Configuración de CORS
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      // Permitir solicitudes desde los orígenes permitidos o solicitudes sin origen (como las de herramientas de prueba)
-      callback(null, true)
-    } else {
-      // Bloquear solicitudes desde orígenes no permitidos
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Permite enviar cookies en solicitudes
-}
-
 app.use(cors(corsOptions))
-app.use((req, res, next) => {
-  const token = req.cookies.access_token
-  req.session = { user: null }
+app.use(session)
 
-  try {
-    const data = jsonwebtoken.verify(token, Secret)
-    req.session.user = data
-  } catch { }
-  next()
-})
-app.get('/', (req, res) => {
-  res.json('hola')
-})
-app.post('/url/create', async (req, res) => {
-  // recuperar la url
-  const { url, id } = req.body
-  if (!url) {
-    res.status(400).json({ error: 'wrong request' })
-  }
-  const shorten = crypto.createHash('md5').update(url).digest('hex').toString()
-  // obtener primeros caracteres
-  const hash = shorten.substring(0, 8)
-  // guardar
-  if (id) {
-    await connection.execute('insert into Url (ShortUrl, OldUrl, UserID_Users) values (?,?,?);', [hash, url, id])
-  } else {
-    await connection.execute('insert into Url (ShortUrl, OldUrl) values (?,?);', [hash, url])
-  }// regresar url shortened
-  res.status(201).json({ shortUrl: `${req.headers.host}/${hash}` })
-})
-
-app.get('/:hash', async (req, res) => {
-  // extraer parametro de la direccion
-  const { hash } = req.params
-  // buscar en la db
-  const [link] = await connection.execute('SELECT OldUrl FROM Url where ShortUrl = ?;', [hash])
-  if (link.length > 0) {
-    // redirigir
-    res.redirect(link[0].OldUrl)
-  }
-  res.status(404)
-})
-
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body
-  try {
-    const [userRows] = await connection.execute('SELECT UserID, Username, Upassword FROM Users WHERE Username = ?', [username])
-    if (userRows.length === 0) throw new Error('Username does not exist')
-    const user = userRows[0]
-    const isValid = await bcrypt.compare(password, user.Upassword)
-    const { Upassword: _, ...authUser } = user
-    if (isValid) {
-      // crear token y guardarlo en las cookies
-      const token = jsonwebtoken.sign({ username: user.Username }, Secret, {
-        expiresIn: '1h'
-      })
-      res.cookie('access_token', token, {
-        httpOnly: true
-        // other cookie settings
-      })
-      res.status(200).json({ authUser })
-    } else {
-      res.status(401).send({ error: 'Invalid password' })
-    }
-  } catch (error) {
-    res.status(401).send({ error: error.message })
-  }
-})
-app.post('/register', async (req, res) => {
-  const { username, email, password } = req.body
-  console.log(req.body)
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10)
-    await connection.execute('insert into Users (Username, Email,Upassword) values (?,?,?);', [username, email, hashedPassword])
-    res.send({ message: 'User create successfully' })
-  } catch (error) {
-    res.status(400).send({ error })
-  }
-})
-app.get('/links/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const [links] = await connection.execute('SELECT id, OldUrl, ShortUrl FROM Url where UserID_Users = ?;', [id])
-
-    if (links.length === '') {
-      return res.status(404).json({ message: 'No posee links activos' })
-    }
-    const formattedLinks = links.map(({ id, OldUrl, ShortUrl }) => ({ id, OldUrl, ShortUrl: `${req.headers.host}/${ShortUrl}` }))
-    res.json(formattedLinks)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Error al obtener los links' })
-  }
-})
-app.delete('/delete/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    await connection.execute('DELETE FROM Url WHERE id = ?;', [id])
-    res.status(204).json({ message: 'Enlace eliminado correctamente' })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Error al eliminar el enlace' })
-  }
-})
-
-app.get('/logout', (req, res) => {
-  res
-    .clearCookie('access_token')
-    .json({ message: 'logout successful' })
-})
+app.use('/', routerUser)
 // Port asignament
 const PORT = process.env.PORT ?? 1234
 
