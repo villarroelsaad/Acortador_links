d8c9310963257a19f8f9eb9214cf1296f49ac088
villarroelsaad <villarroelsaad@gmail.com>
2024-09-02 16:40:28 -0400

    v

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,12 +9,12 @@ export const Home = () => {
   const { user } = useContext(UserContext)
   return (
     <section className='flex w-full flex-wrap'>
-      <p className='text-slate-200  w-full opacity-85 text-base self-end text-end'>Bienvenido {user.authUser.Username}</p>
+      <p className='text-slate-200  w-full opacity-85 text-base font-semibold self-end ml-5 text-end'>Bienvenido <span className='text-bold'> {user.authUser.Username}</span></p>
       <div className='w-1/4'>
         <NavBar />
       </div>
 
-      <article className='flex justify-center w-3/4'>
+      <article className='flex justify-center w-full'>
         <Outlet />
         {location.pathname === '/home' && <Shortener />}
 
