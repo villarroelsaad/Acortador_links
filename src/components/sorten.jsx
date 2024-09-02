97eb8a28a9030f52fd706ef6f9e1b90ef21d9e87
villarroelsaad <villarroelsaad@gmail.com>
2024-09-02 16:32:59 -0400

    log

--- src/components/sorten.jsx
+++ src/components/sorten.jsx
@@ -3,7 +3,7 @@ export const Sorten = () => {
   return (
     <section className='flex flex-wrap flex-col'>
       <Shortener />
-      <a href='/Login' className='bg-violet-500 self-center mt-14 text-slate-200 bg-opacity-90 w-28 p-2 text-center rounded-lg font-semibold'>Iniciar sesion</a>
+      <a href='/Login' className='bg-violet-500 self-center mt-20 text-slate-200 bg-opacity-90 w-28 p-2 text-center rounded-lg font-semibold'>Iniciar sesion</a>
     </section>
   )
 }
