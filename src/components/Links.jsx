e8be1ae1a19e01e3a2eb6e094769093cecc3c2d9
villarroelsaad <villarroelsaad@gmail.com>
2024-09-03 08:58:12 -0400

    links desing

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -36,18 +36,18 @@ export const Links = () => {
 
     <section className=''>
       <h1 className='text-center font-semibold text-5xl mb-20  mt-20 text-slate-100 opacity-90'>Links</h1>
-      <div className='flex flex-wrap w-full gap-10'>{links.map(url => (
+      <div className='flex flex-wrap justify-center w-full gap-10'>{links.map(url => (
         <li className=' list-none ' key={url.id}>
-          <div className=' bg-zinc-900 w-1/2 border-2 border-zinc-800 text-center rounded-3xl'>
+          <div className=' bg-zinc-900 w-auto border-2 border-zinc-800 text-center rounded-3xl'>
             <div className='flex flex-col p-5 '>
               <p className='text-slate-200  opacity-85 font-semibold'><span className='text-violet-500 opacity-90'>Url Original:</span><br /> {url.OldUrl}</p>
               <br />
               <p className='text-slate-200 opacity-85 font-semibold'><span className='text-violet-500 opacity-90'>Url Acortada:</span><br /> {url.ShortUrl}</p>
               <button
                 onClick={() => handleDelete(url.id)}
-                className='w-7 flex justify-center self-end rounded-full bg-gray-400 hover:opacity-70'
+                className='w-7 flex justify-center mt-2 self-end rounded-full bg-zinc-800 hover:opacity-70'
               >
-                <img className='w-5 h-7 t' src='/delete.svg' alt='' />
+                <img className='w-5 h-7 ' src='/delete.svg' alt='' />
               </button>
             </div>
           </div>
